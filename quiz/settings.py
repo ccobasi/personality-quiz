1b7171f1a33c522248d32a9d178caa35bee72216
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-08-18 15:18:16 +0100

    made migration and create super user

--- quiz/settings.py
+++ quiz/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'personality',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
