0ff471399bd805a5966b3482a02dd8e6b1c5b0bd
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-08-20 18:04:48 +0100

    add views index

--- personality/views.py
+++ personality/views.py
@@ -1,8 +1,13 @@
 from re import template
 from django.shortcuts import render
 from .models import *
-from django.views.generic import ListView
+# from django.views.generic import ListView
+from django.http import HttpResponse
 
 
-class QuizListView(ListView):
-    pass
+# class QuizListView(ListView):
+#     pass
+
+def index(request):
+    html = "<html><body><h1>Hello World</h1></body></html>"
+    return HttpResponse(html)
--- quiz/urls.py
+++ quiz/urls.py
@@ -1,9 +1,14 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+
+import personality
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('personality/', include(personality.urls, namespace="personality"))
 ]
 
 # urlpatterns+= static(settings.STATIC_URL, document_root=settings.)
