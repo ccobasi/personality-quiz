d92e89a699bab216b90d9685ebe22f49b0544906
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-08-23 21:43:25 +0100

    add home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-import Header from "./components/Header";
+import { Routes,Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Routes>
+      <Route path="/" element={<Home />}>
+          
+      </Route>
+      </Routes>
     </div>
   );
 }
